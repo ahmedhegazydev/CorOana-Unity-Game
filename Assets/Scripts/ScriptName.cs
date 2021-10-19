00ca1581b3326433119f6885e556370c9fea327f
ahmedhegazy <ahmed.hegazy@safavisa.com>
2021-10-19 16:26:41 +0200

    adding the speed scrip component for charachter_01

---
+++ Assets/Scripts/ScriptName.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptName : MonoBehaviour
+{
+    public float speed = 5;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector2 pos = transform.position;
+
+        pos.x += speed * h * Time.deltaTime;
+        pos.y += speed * v * Time.deltaTime;
+
+        transform.position = pos;
+
+
+    }
+}
